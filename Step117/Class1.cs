1e4e95f5d9804d0bfde1fe67ee374c053835eb79
Cnix24 <calvinattl24@gmail.com>
2019-07-18 12:50:44 -0600

    Update

---
+++ Step117/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step117
+{
+    public class Class1
+    {
+        //Step 6: static class
+        public static string secretMessage;
+        public Class1()
+        {
+            secretMessage = "Hi there!";
+        }
+  
+        public void VoidMethod(int x, out int y)
+        {
+            y = x / 2;
+        }
+
+        public void VoidMethod(int x, int z, out int y)
+        {
+            int a;
+            a = x + z;
+            y = a / 2;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Step117/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Step117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Steps 1-4 | Create a class and void method with an output integer. Method divides input by 2. Method uses output parameters.
+
+            int x;
+
+            Class1 class1 = new Class1();
+
+            Console.WriteLine("Please provide a whole number: ");
+            string firstInput = Console.ReadLine();
+            int firstInteger = Convert.ToInt32(firstInput);
+
+            class1.VoidMethod(firstInteger, out x);
+            
+
+            Console.WriteLine("The number you provided divided by 2 equals "+x);
+            Console.ReadLine();
+
+            //END: Steps 1-4
+
+            // Step 5 | Overload a method
+
+            int y; 
+            Class1 classB = new Class1();
+
+            Console.WriteLine("Please provide a whole number: ");
+            string secondInput = Console.ReadLine();
+            int secondInteger = Convert.ToInt32(secondInput);
+
+            Console.WriteLine("Please provide a second whole number: ");
+            string thirdInput = Console.ReadLine();
+            int thirdInteger = Convert.ToInt32(thirdInput);
+
+            classB.VoidMethod(secondInteger, thirdInteger, out y);
+
+            
+
+            Console.WriteLine("The sum of those numbers divided by 2 equals " + y);
+            Console.ReadLine();
+
+            //END: Step 5
+
+            //Step 6: static class
+
+            Console.WriteLine(Class1.secretMessage);
+            Console.ReadLine();
+
+            //Step 6: end
+
+        }
+    }
+}
